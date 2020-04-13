13022ba5132e66b0a058d7915eb5bd0a027d32b7
陈泽槟 <chenzebing@wps.cn>
2020-04-13 11:33:01 +0800

    事件驱动模式

---
+++ 事件驱动模型/BEventLoop/BEventLoop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BEventLoop
+//
+//  Created by ChenZeBin on 2020/3/27.
+//  Copyright © 2020 ChenZeBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 事件驱动模型/BEventLoop/BEventLoop/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BEventLoop
+//
+//  Created by ChenZeBin on 2020/3/27.
+//  Copyright © 2020 ChenZeBin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
