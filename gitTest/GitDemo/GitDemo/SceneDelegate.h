4f8959bf7fd80d27d699821f0a5e1d6576c2b7ad
陈泽槟 <chenzebing@bytedance.com>
2020-08-07 11:54:59 +0800

    文章 (+1 squashed commit) Squashed commits: [3594e39] gitTest

---
+++ gitTest/GitDemo/GitDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  GitDemo
+//
+//  Created by bytedance on 2020/8/7.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ gitTest/GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by bytedance on 2020/8/7.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
