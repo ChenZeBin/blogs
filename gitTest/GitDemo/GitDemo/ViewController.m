3594e3936ad9b6fc697302f90a019f4dd467c9d3
陈泽槟 <chenzebing@bytedance.com>
2020-08-07 11:54:59 +0800

    gitTest

---
+++ gitTest/GitDemo/GitDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by bytedance on 2020/8/7.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ gitTest/GitDemo/GitDemo/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  GitDemo
+//
+//  Created by bytedance on 2020/8/7.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
